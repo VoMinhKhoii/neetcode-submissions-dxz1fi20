faca21645223b64e160e0e9494135c4442ae4583
Vo Minh Khoi <s3991730@rmit.edu.vn>
2026-04-14 09:29:35 +0700

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.py
@@ -0,0 +1,33 @@
+import sys
+import math
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        largest_pile = -sys.maxsize - 1
+        min_k = sys.maxsize + 1
+        for i in piles:
+            if i >= largest_pile:
+                largest_pile = i
+        
+        # UPPER BOUND
+        # k = largest_pile when h = piles.length 
+        left, right = 1, largest_pile
+
+        while left <= right:
+            k = math.ceil((left + right) / 2)
+            h_with_current_k = 0
+            for i in piles:
+                h_with_current_k += math.ceil(i / k)
+
+            if h_with_current_k == h:
+                return k
+
+            if h_with_current_k > h:
+                left = k + 1
+            if h_with_current_k < h:
+                right = k - 1
+                min_k = k
+        return min_k
+            
+
+        
